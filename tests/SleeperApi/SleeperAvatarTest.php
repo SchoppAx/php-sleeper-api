830a01493dc4f3d932e24f32487e1a653c4f3ae7
Axel <develop4all@gmail.com>
2021-11-27 16:27:18 +0100

    Change test to live return code

--- tests/SleeperApi/SleeperAvatarTest.php
+++ tests/SleeperApi/SleeperAvatarTest.php
@@ -7,27 +7,39 @@ use SchoppAx\Sleeper\Api\Avatars;
 class SleeperAvatarTest extends TestCase
 {
 
+  private function remoteFileExists($url) {
+    $curl = curl_init($url);
+
+    //don't fetch the actual page, you only want to check the connection is ok
+    curl_setopt($curl, CURLOPT_NOBODY, true);
+
+    $result = curl_exec($curl);
+    $ret = false;
+
+    //if request did not fail
+    if ($result !== false) {
+      //if request was ok, check response code
+      $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+      if ($statusCode == 200) {
+        $ret = true;
+      }
+    }
+
+    curl_close($curl);
+
+    return $ret;
+  }
+
   public function testAvatar()
   {
-    $value = __DIR__ . '/data/740715586f5fecd032030346acf139c5.png';
+    $client = new SleeperClient();
 
-    $mock = $this->createMock(Avatars::class);
-    $mock->expects($this->once())
-      ->method('find')
-      ->willReturnCallback(function() use ($value) {
-        return $value;
-      });
+    $url = $client->avatars()->find('79f5f8ecdd59fe94b668993366552b3a');
+    $exists = $this->remoteFileExists($url);
 
-    $client = $this->createPartialMock(SleeperClient::class, ['__call']);
-    $client->expects($this->once())
-      ->method('__call')
-      ->willReturnCallback(function() use ($mock) {
-        return $mock;
-      });
-
-    $response = $client->avatars()->find('gg');
-
-    $this->assertEquals('image/png', mime_content_type($response));
+    $this->assertIsBool($exists);
+    $this->assertEquals($exists, true);
   }
 
 }
